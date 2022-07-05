8b9ac3d7c888d99c9669da89dc8b5b0057196373
Shohzod <1505shakhzod@gmial.com>
2022-07-05 16:42:27 +0300

    SharedPref
    SharedPref

---
+++ app/src/main/java/ru/itis/bozor/fragments/PurchaseFragment.kt
@@ -0,0 +1,22 @@
+package ru.itis.bozor.fragments
+
+import android.os.Bundle
+import android.view.View
+import androidx.fragment.app.Fragment
+import ru.itis.bozor.R
+import ru.itis.bozor.databinding.FragmentPurchaseBinding
+
+class PurchaseFragment: Fragment(R.layout.fragment_purchase) {
+    private var _binding: FragmentPurchaseBinding? = null
+    private val binding get() = _binding!!
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        _binding = FragmentPurchaseBinding.bind(view)
+    }
+
+    override fun onDestroyView() {
+        _binding = null
+        super.onDestroyView()
+    }
+}
---
+++ app/src/main/java/ru/itis/bozor/fragments/SignUpFragment.kt
@@ -0,0 +1,41 @@
+package ru.itis.bozor.fragments
+
+import android.content.Context
+import android.os.Bundle
+import android.view.View
+import androidx.core.content.edit
+import androidx.fragment.app.Fragment
+import androidx.navigation.fragment.findNavController
+import ru.itis.bozor.R
+import ru.itis.bozor.databinding.FragmentSignupBinding
+
+class SignUpFragment: Fragment(R.layout.fragment_signup) {
+    private var _binding: FragmentSignupBinding? = null
+    private val binding get() = _binding!!
+    val pref = activity?.getSharedPreferences("REGISTRATION",Context.MODE_PRIVATE)
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        _binding = FragmentSignupBinding.bind(view)
+        val bMenu = activity!!.findViewById<View>(R.id.bNav)
+        bMenu.visibility = View.GONE
+
+        binding.btCreate.setOnClickListener {
+            if(binding.tiSignUpUsername.editText?.text.toString() != ""){
+                findNavController().navigate(R.id.action_signUpFragment_to_shopFragment)
+                bMenu.visibility = View.VISIBLE
+
+                pref?.edit {
+                    putString("USERNAME", binding.tiSignUpUsername.editText?.text.toString())
+                    commit()
+                }
+            }
+        }
+    }
+
+    override fun onDestroyView() {
+        _binding = null
+        super.onDestroyView()
+    }
+
+}
